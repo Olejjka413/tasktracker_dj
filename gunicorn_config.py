6397967a18670ea9b6d004fe4c564efd45debf24
Olejjka413 <Ivanova.Olya2001@yandex.ru>
2022-07-07 18:38:13 +0300

    deploy politech

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/home/std/<название git репозитория, например django-web-project>/venv/bin/gunicorn'
-pythonpath = '/home/std/<название git репозитория>'
+command = '/home/std/<tasktracker_dj>/venv/bin/gunicorn'
+pythonpath = '/home/std/<tasktracker_dj>'
 bind="127.0.0.1:8000"
 workers = 3
